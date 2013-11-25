a109a25e6c8d0f8be777466deee456ef23ebfd66
jackhanford <jackhanford@gmail.com>
2013-11-25 15:56:59 -0800

    added buttons that switch the class

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,16 @@
-$(function() {
-  $('#slider').slider();
+$(document).ready(function() {
+  $('.btn-blue').click(function() {
+    $('.header-green').addClass('header-blue').removeClass("header-green header-red")
+    $('.header-red').addClass('header-blue').removeClass("header-green header-red")
+  });
+
+  $('.btn-red').click(function() {
+    $('.header-green').addClass('header-red').removeClass("header-green header-blue")
+    $('.header-blue').addClass('header-red').removeClass("header-green header-blue")
+  });
+
+  $('.btn-green').click(function() {
+    $('.header-blue').addClass('header-green').removeClass("header-red header-blue")
+    $('.header-red').addClass('header-green').removeClass("header-red header-blue")
+  });
 });
