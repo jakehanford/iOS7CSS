62141798d473f8236fb56ee00d8aedf4ffc7eb60
jackhanford <jackhanford@gmail.com>
2013-11-26 13:36:29 -0800

    Renamed all headers to navs

--- js/javascript.js
+++ js/javascript.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
   $('.btn-blue').click(function() {
-    $('.header-green').addClass('header-blue').removeClass("header-green header-red header-white")
-    $('.header-red').addClass('header-blue').removeClass("header-green header-red header-white")
-    $('.header-white').addClass('header-blue').removeClass("header-green header-red header-white")
+    $('.nav-green').addClass('nav-blue').removeClass("nav-green nav-red nav-white")
+    $('.nav-red').addClass('nav-blue').removeClass("nav-green nav-red nav-white")
+    $('.nav-white').addClass('nav-blue').removeClass("nav-green nav-red nav-white")
   });
 
   $('.btn-red').click(function() {
-    $('.header-green').addClass('header-red').removeClass("header-green header-blue header-white")
-    $('.header-blue').addClass('header-red').removeClass("header-green header-blue header-white")
-    $('.header-white').addClass('header-red').removeClass("header-green header-blue header-white")
+    $('.nav-green').addClass('nav-red').removeClass("nav-green nav-blue nav-white")
+    $('.nav-blue').addClass('nav-red').removeClass("nav-green nav-blue nav-white")
+    $('.nav-white').addClass('nav-red').removeClass("nav-green nav-blue nav-white")
   });
 
   $('.btn-green').click(function() {
-    $('.header-blue').addClass('header-green').removeClass("header-red header-blue header-white")
-    $('.header-red').addClass('header-green').removeClass("header-red header-blue header-white")
-    $('.header-white').addClass('header-green').removeClass("header-red header-blue header-white")
+    $('.nav-blue').addClass('nav-green').removeClass("nav-red nav-blue nav-white")
+    $('.nav-red').addClass('nav-green').removeClass("nav-red nav-blue nav-white")
+    $('.nav-white').addClass('nav-green').removeClass("nav-red nav-blue nav-white")
   });
 
   $('.btn-white').click(function() {
-    $('.header-blue').addClass('header-white').removeClass("header-red header-blue header-green")
-    $('.header-red').addClass('header-white').removeClass("header-red header-blue header-green")
-    $('.header-green').addClass('header-white').removeClass("header-red header-blue header-green")
+    $('.nav-blue').addClass('nav-white').removeClass("nav-red nav-blue nav-green")
+    $('.nav-red').addClass('nav-white').removeClass("nav-red nav-blue nav-green")
+    $('.nav-green').addClass('nav-white').removeClass("nav-red nav-blue nav-green")
   });
 });
