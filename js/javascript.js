c7b33333501d1b4bd388dd2e90059725638c88d4
jackhanford <jackhanford@gmail.com>
2013-11-25 17:18:44 -0800

    Big changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
   $('.btn-blue').click(function() {
-    $('.header-green').addClass('header-blue').removeClass("header-green header-red")
-    $('.header-red').addClass('header-blue').removeClass("header-green header-red")
+    $('.header-green').addClass('header-blue').removeClass("header-green header-red header-white")
+    $('.header-red').addClass('header-blue').removeClass("header-green header-red header-white")
+    $('.header-white').addClass('header-blue').removeClass("header-green header-red header-white")
   });
 
   $('.btn-red').click(function() {
-    $('.header-green').addClass('header-red').removeClass("header-green header-blue")
-    $('.header-blue').addClass('header-red').removeClass("header-green header-blue")
+    $('.header-green').addClass('header-red').removeClass("header-green header-blue header-white")
+    $('.header-blue').addClass('header-red').removeClass("header-green header-blue header-white")
+    $('.header-white').addClass('header-red').removeClass("header-green header-blue header-white")
   });
 
   $('.btn-green').click(function() {
-    $('.header-blue').addClass('header-green').removeClass("header-red header-blue")
-    $('.header-red').addClass('header-green').removeClass("header-red header-blue")
+    $('.header-blue').addClass('header-green').removeClass("header-red header-blue header-white")
+    $('.header-red').addClass('header-green').removeClass("header-red header-blue header-white")
+    $('.header-white').addClass('header-green').removeClass("header-red header-blue header-white")
+  });
+
+  $('.btn-white').click(function() {
+    $('.header-blue').addClass('header-white').removeClass("header-red header-blue header-green")
+    $('.header-red').addClass('header-white').removeClass("header-red header-blue header-green")
+    $('.header-green').addClass('header-white').removeClass("header-red header-blue header-green")
   });
 });
